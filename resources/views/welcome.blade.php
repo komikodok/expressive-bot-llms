30baf68a17dd8c95920fc47643c1917269d07da5
komikodok <komikodok666@gmail.com>
2024-11-18 22:23:33 +0700

    add SocialiteController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Laravel')
     
 @section('content')
-    <div class="bg-gray-300 w-screen h-screen"></div>
+    <div class="bg-gray-300 w-screen h-screen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ducimus exercitationem laborum corrupti repellendus tempora nulla vel, illo deserunt rem necessitatibus, culpa similique provident aliquam odio fugit cum, quidem tenetur!</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SocialiteController;
 use App\Http\Controllers\Welcome\WelcomeController;
 
 Route::get('/', [WelcomeController::class, 'index']);
+Route::get('auth/google/redirect', [SocialiteController::class, 'redirect'])->middleware('guest');
+Route::get('auth/google/callback', [SocialiteController::class, 'callback'])->middleware('guest');
