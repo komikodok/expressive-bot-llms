a565c71ff251a0b8beac971af5a97d4555a84b61
komikodok <komikodok666@gmail.com>
2024-11-21 10:57:07 +0700

    minor update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\SocialiteController;
-use App\Http\Controllers\Welcome\WelcomeController;
 
-Route::get('/', [WelcomeController::class, 'index'])->name('index');
+Route::get('/', [ChatbotController::class, 'index'])->name('index');
 Route::get('auth/google/redirect', [SocialiteController::class, 'redirect'])->middleware('guest')->name('google.redirect');
 Route::get('auth/google/callback', [SocialiteController::class, 'callback'])->middleware('guest')->name('google.callback');
