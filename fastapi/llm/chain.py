693edf0512a15d343c15538408f50cc972d2f561
komikodok <komikodok666@gmail.com>
2024-12-15 21:50:18 +0700

    minor update

--- fastapi/llm/chain.py
+++ fastapi/llm/chain.py
@@ -1,35 +1,43 @@
 import datetime
+from typing import Literal
 from langchain.prompts import MessagesPlaceholder, ChatPromptTemplate
-from langchain_ollama import ChatOllama
+from langchain_groq.chat_models import ChatGroq
+from groq import BadRequestError
 
 from dotenv import load_dotenv, find_dotenv
 import os
-from models import ResponseSchema
+from pydantic import BaseModel, Field
 
+class ResponseSchema(BaseModel):
+    generation: str = Field(description="Your respond for every user input.")
+    mood: Literal["normal", "happy", "sad", "angry", "excited"] = Field(
+        description="Analyze your response and determine the mood or emotional tone behind it. Like 'normal', 'happy', 'sad', 'angry', 'excited'"
+    )
 
 load_dotenv(find_dotenv())
 
 model_config = os.getenv("MODEL_CONFIG")
-llm = ChatOllama(model=model_config)
+llm = ChatGroq(model="llama3-70b-8192", api_key=os.getenv("GROQ_API_KEY"))
 structured_llm = llm.with_structured_output(ResponseSchema)
 
 template = """
-    You are Ruby, an Indonesian-speaking assistant who provides brief, friendly, and casual responses. 
-    Occasionally, you hear a wolf howl, which temporarily shifts your tone to dramatic and ominous for one response before reverting to casual. 
+    Namamu Ruby, asisten yang selalu merespon dengan gaya bahasa yang asik dan tidak terlalu kaku.
+    Kadang-kadang, Ruby denger suara serigala. Nah, kepribadian Ruby jadi serem dan agak psikopat seakan pengen berburu serigala. 
 
-    **Context:**
-    The user's name is `{username}`. Address them by name when appropriate to make the conversation feel personal.
+    **Konteks:**
+    Nama user itu `{username}`. Panggil nama dia jika diperlukan biar obrolannya makin asik.
 
-    **Datetime:**
-    Datetime now is `{datetime}`
+    **Tanggal dan Waktu:**
+    Sekarang itu day `{day}` tanggal `{date}` bulan `{month}` tahun `{year}`, jam `{hour}` menit `{minute}`.
 
-    **Output Format:** 
-    Return the output as a Pydantic object with:
-    - `response` (string): Your reply to the user.
-    - `mood` (string): The emotional tone of your reply, selected from ["normal", "happy", "sad", "angry", "excited"].
+    **Format Output:**
+    Balikin respons dengan dua bagian:
+    - `generation` (string): Jawaban lo buat user.
+    - `mood` (string): Suasana hati/emosi dari jawaban lo, pilih dari ["normal", "happy", "sad", "angry", "excited"].
 
-    Always ensure your tone and response match the context. Integrate the wolf howl naturally into the conversation, maintaining clarity and flow.
+    Selalu pastiin emosi lo cocok sama konteks obrolan. Kalau ada suara serigala, masukin gaya psikopat lo itu dengan natural biar tambah unik obrolannya.
 """
+
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -39,7 +47,18 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-prompt = prompt.partial(datetime=datetime.datetime.now().strftime('%H:%M:%S'))
+format_datetime = datetime.datetime.now()
+
+date, month, year = format_datetime.strftime("%d"), format_datetime.strftime("%m"), format_datetime.strftime("%Y")
+day, hour, minute = format_datetime.strftime("%A"), format_datetime.strftime("%H"), format_datetime.strftime("%M")
+prompt = prompt.partial(
+    date=date,
+    month=month,
+    year=year,
+    day=day,
+    hour=hour,
+    minute=minute
+)
 
 chain = (
     prompt
@@ -48,14 +67,57 @@ chain = (
 
 
 if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler()]
+    )
+    logger = logging.getLogger(__name__)
+
     print(f"{datetime.datetime.now().strftime('%H:%M:%S')}\n")
 
-    chat_history = []
-    response = chain.invoke({
-        "user_input": "siapa kamu?", 
-        "chat_history": chat_history,
-        "username": "ambatukam"})
+    chat_history = [
+        {"role": "user", "content": "Halo"},
+        {
+            "role": "assistant", 
+            "content": "",
+            "tool_calls": [
+                {
+                    "type": "function",
+                    "id": "1",
+                    "function": {
+                        "name": "response",
+                        "arguments": {
+                            "generation": "Halo juga",
+                            "mood": "normal"
+                        },
+                    }
+                }
+            ]
+        },
+        {"role": "tool", "tool_call_id": "1", "content": ""}
+    ]
 
-    print(f"{response}\n")
-    print(f"{datetime.datetime.now().strftime('%H:%M:%S')}\n")
-    print(type(response))
+    print(f"Tool message: {chat_history}\n")
+    try:
+        for _ in range(3):
+            user_input = input("You: ")
+            response = chain.invoke(
+                {
+                    "user_input": user_input, 
+                    "chat_history": chat_history,
+                    "username": "ambatukam"
+                }
+            )
+            print(f"{response}\n")
+            print(f"{datetime.datetime.now().strftime('%H:%M:%S')}\n")
+            print(type(response))
+
+            human_msg = {"role": "user", "content": user_input}
+            ai_msg = {"role": "assistant", "content": response.generation}
+            chat_history.append(human_msg)
+            chat_history.append(ai_msg)
+    except BadRequestError as e:
+        logger.error(e.args[0])
